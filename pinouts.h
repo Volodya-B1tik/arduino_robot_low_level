1fc6e3a884984e0013f64e8e2512677fb9d726f2
Vladimir Semenov <96019021+Volodya-B1tik@users.noreply.github.com>
2023-07-07 14:55:19 +0300

    Update pinouts.h

--- pinouts.h
+++ pinouts.h
@@ -4,65 +4,49 @@
 #define In_2 3 //
 #define In_1 4 //
 #define EnA 5 // Мотор правой гусеницы
-#define pi 3.141592653589793238462643383279
+//#define pi 3.141592653589793238462643383279
+
+//IMU
+unsigned long currentMillis;
+unsigned long prevMillis;
 
 //right enco
-#define inB 4
-#define inA 5
-volatile long pause1    = 25;  // Пауза для борьбы с дребезгом
-volatile long lastTurn1 = 0;   // Переменная для хранения времени последнего изменения
+#define ENC_A_l  19
+#define ENC_B_l  18
+#define ENC_A_r  2
+#define ENC_B_r  3
+volatile boolean state_a_l = 0;
+volatile boolean state_b_l = 0;
+volatile boolean state_a_r = 0;
+volatile boolean state_b_r = 0;
 
-volatile int count1 = 0;       // Счетчик оборотов
-int actualcount1    = 0;       // Временная переменная определяющая изменение основного счетчика
+// Encoder position
+volatile int enc_pos_l = 0;
+int enc_pos_prev_l = 0;
+int enc_pos_change_l = 0;
 
-volatile int state1 = 0;       // Статус одного шага - от 0 до 4 в одну сторону, от 0 до -4 - в другую
-
-volatile int inAValue = 0;   // Переменные хранящие состояние пина, для экономии времени
-volatile int inBValue = 0;   // Переменные хранящие состояние пина, для экономии времени
-
-
-// Left Enco
-#define inB1 3
-#define inA1 2
-volatile long pause    = 25;  // Пауза для борьбы с дребезгом
-volatile long lastTurn = 0;   // Переменная для хранения времени последнего изменения
-
-volatile int count = 0;       // Счетчик оборотов
-int actualcount    = 0;       // Временная переменная определяющая изменение основного счетчика
-
-volatile int state = 0;       // Статус одного шага - от 0 до 4 в одну сторону, от 0 до -4 - в другую
-
-volatile int inA1Value = 0;   // Переменные хранящие состояние пина, для экономии времени
-volatile int inB1Value = 0;   // Переменные хранящие состояние пина, для экономии времени
-
+volatile int enc_pos_r = 0;
+int enc_pos_prev_r = 0;
+int enc_pos_change_r = 0;
 /////////////////////////////////////////////////
 
 #define trigPin_left 10
 #define echoPin_left 11
-//int trigPin_mid = 8;
-//int echoPin_mid = 9;
-//int trigPin_right = 6;
-//int echoPin_right = 7;
 #define trigPin_mid 6
 #define echoPin_mid 7
 #define trigPin_right 8
 #define echoPin_right  9
 
 // Энкодер правой гусеницы
-int counter_right = 0;
-int current_state_right;
-int pre_state_right;
-
-// Энкодер левой гусеницы
-float counter_left = 0;
-float current_state_left;
-float pre_state_left;
 
 int duration_left , distance_left, duration_mid , distance_mid, duration_right , distance_right;
+
+
 //int N=4000;// Мощность двигателя
 double gradus; // Градус поворота
 float radian; // градус в радианах
 
+int left = 0, mid = 0, right = 0;
 
 int sonar_data[3];
 #define DEBUG true
